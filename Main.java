ba01eeb1af8bb3a2eca2230d29a2a39a61fc7e78
Your Name <acolllen@myuax.com>
2026-09-15 16:21:24 +0200

    Ejecución comprobada de miBicicleta y tuBicicleta, y compila sin errores después de meter la clase bicicleta en un paquete y ejecutar desde fuera con el main

--- Main.java
+++ Main.java
@@ -1,2 +1,18 @@
+import claseBicicleta.Bicicleta;
+
 public class Main {
+    public static void main(String[] args) {
+        Bicicleta miBicicleta = new Bicicleta(10,2,5);
+        Bicicleta tuBicicleta = new Bicicleta(15,3,4);
+
+        miBicicleta.acelerar();
+        tuBicicleta.frenar();
+        miBicicleta.cambiarPlato(6);
+        tuBicicleta.cambiarPiñon(7);
+
+        System.out.println(miBicicleta);
+        System.out.println(tuBicicleta);
+    }
+
+
 }
